d90b2fe2883dc350a91b13fdc5e095b0f80e4720
whitehatjranish <62799020+whitehatjranish@users.noreply.github.com>
2020-04-28 23:48:43 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var raindrop,raindrop1,raindrop2,raindrop3,raindrop4,raindrop5,raindrop6,raindrop7,raindrop8,raindrop9,raindrop10,raindrop11,raindrop12,raindrop13,raindrop14;
+
+
+function setup() {
+  createCanvas(800,400);
+  raindrop=  createSprite(400, 200, 3, 50);
+  raindrop.shapeColor="blue";
+  raindrop1=  createSprite(300, 50, 3, 50);
+  raindrop1.shapeColor="pink";
+  raindrop2=  createSprite(200, 100, 3, 50);
+  raindrop2.shapeColor="purple";
+  raindrop3=  createSprite(500, 250, 3, 50);
+  raindrop3.shapeColor="purple";
+  raindrop4=  createSprite(600, 150, 3, 50);
+  raindrop4.shapeColor="red";
+  raindrop5=  createSprite(700, 220, 3, 50);
+  raindrop5.shapeColor="yellow";
+  raindrop6=  createSprite(450, 300, 3, 50);
+  raindrop6.shapeColor="orange";
+  raindrop7=  createSprite(550, 320, 3, 50);
+  raindrop7.shapeColor="brown";
+  raindrop8=  createSprite(650, 280, 3, 50);
+  raindrop8.shapeColor="green";
+  raindrop9=  createSprite(750, 360, 3, 50);
+  raindrop9.shapeColor="blue";
+  raindrop10=  createSprite(100, 220, 3, 50);
+  raindrop10.shapeColor="yellow";
+  raindrop11=  createSprite(250, 300, 3, 50);
+  raindrop11.shapeColor="orange";
+  raindrop12=  createSprite(280, 320, 3, 50);
+  raindrop12.shapeColor="brown";
+  raindrop13=  createSprite(120, 280, 3, 50);
+  raindrop13.shapeColor="blue";
+  raindrop14=  createSprite(480, 360, 3, 50);
+  raindrop14.shapeColor="pink";
+}
+
+function draw() {
+  background("green");  
+  raindrop.display();
+  raindrop1.display();
+  raindrop2.display();
+  raindrop3.display();
+  raindrop4.display();
+  raindrop5.display();
+  raindrop6.display();
+  raindrop7.display();
+  raindrop8.display();
+  raindrop9.display();
+  raindrop10.display();
+  raindrop11.display();
+  raindrop12.display();
+  raindrop13.display();
+  raindrop14.display();
+  drawSprites();
+  }
+    
+
+
+ 
